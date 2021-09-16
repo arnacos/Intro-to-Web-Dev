425cc2eeb1fecbb9b1835d836fda462a2fd6b0f4
Arn Acosta <arnben17@gmail.com>
2021-09-16 15:26:37 -0600

    Testing Java using IDE

---
+++ Milestone 1/Code Along/HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author: Arn Acosta
+ * email: arnben17@gmail.com
+ * Date Created: 09/06/2021
+ * Purpose: My first Hello World program in a IDE
+ */
+public class Hello {
+    public static void main(String[]args){
+        System.out.println("Hello, World!");
+    }
+    
+}
